1f0f422a32475fd8870ca89be46400c708bca376
Samuel Rocha Gil <samugil@gmail.com>
2023-08-08 14:56:51 +0100

    Room Application

---
+++ app/src/main/java/com/app/coinwise/CoinWiseApplication.kt
@@ -0,0 +1,19 @@
+package com.app.coinwise
+
+import android.app.Application
+import androidx.room.Room
+import com.app.coinwise.data.local.AppDataBase
+
+class CoinWiseApplication: Application() {
+
+
+    lateinit var dataBase: AppDataBase
+    override fun onCreate() {
+        super.onCreate()
+
+        dataBase = Room.databaseBuilder(
+            applicationContext,AppDataBase::class.java, "coinwise-database"
+        ).build()
+
+    }
+}
