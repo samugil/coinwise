566224ec822a50055eba5a567556af7aa2e0242f
giovannasreis <giovannareis20@outlook.com>
2023-08-08 16:14:46 +0100

    Criando Botões

--- app/src/main/java/com/app/coinwise/MainActivity.kt
+++ app/src/main/java/com/app/coinwise/MainActivity.kt
@@ -2,10 +2,16 @@ package com.app.coinwise
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.DropBoxManager
+import java.security.KeyStore.Entry
+import java.util.ArrayList
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
+
 }
+
